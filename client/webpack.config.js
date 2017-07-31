63961f6d38f55d066d7f132920ffda66de7ae9ce
ssmith-wombatweb <ssmith@wombatweb.us>
2017-07-30 20:09:28 -0700

    Updated dev settings.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,16 +1,17 @@
-var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const cssNano = require('cssnano');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: "dist/assets",
-    filename: "bundle.js",
-    publicPath: "assets"
+    path: 'dist/assets',
+    filename: 'bundle.js',
+    publicPath: 'assets',
   },
   devServer: {
     inline: true,
     contentBase: './dist',
-    port: 3000
+    port: 3000,
   },
   module: {
     loaders: [
@@ -25,25 +26,25 @@ module.exports = {
       {
         test: /\.json$/,
         exclude: /(node_modules)/,
-        loader: 'json-loader'
+        loader: 'json-loader',
       },
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader!autoprefixer-loader'
+        loader: 'style-loader!css-loader!autoprefixer-loader',
 
       },
       {
         test: /\.scss/,
-        loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
-      }
-    ]
+        loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader',
+      },
+    ],
   },
   plugins: [
     new OptimizeCssAssetsPlugin({
       assetNameRegExp: /\.optimize\.css$/g,
-      cssProcessor: require('cssnano'),
+      cssProcessor: cssNano,
       cssProcessorOptions: { discardComments: { removeAll: true } },
-      canPrint: true
-    })
-  ]
-}
+      canPrint: true,
+    }),
+  ],
+};
