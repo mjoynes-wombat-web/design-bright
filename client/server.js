b9d803eeb88dea3ef3272d88e2a4ee67b77de132
ssmith-wombatweb <ssmith@wombatweb.us>
2017-08-16 08:31:18 -0700

    Added http redirect.

--- client/server.js
+++ client/server.js
@@ -3,9 +3,10 @@ import path from 'path';
 import dotenv from 'dotenv';
 import logger from 'morgan';
 import fs from 'fs';
+import http from 'http';
 import https from 'https';
 
-const { PORT = 3001, STATUS, HOST = '0.0.0.0' } = dotenv.config().parsed;
+const { HTTP_PORT = 3001, HTTPS_PORT = 3002, STATUS, HOST = '0.0.0.0' } = dotenv.config().parsed;
 
 const app = express();
 
@@ -24,10 +25,19 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
+http.createServer((req, res) => {
+  console.log('HTTP redirects to HTTPS');
+  const hostname = (req.headers.host.match(/:/g)) ? req.headers.host.slice(0, req.headers.host.indexOf(':')) : req.headers.host;
+  const redirect = `https://${hostname}:${HTTPS_PORT}${req.url}`;
+  console.log(redirect);
+  res.writeHead(301, { Location: redirect });
+  res.end();
+}).listen(HTTP_PORT, HOST);
+
 https.createServer({
   key: fs.readFileSync('./private.key'),
   cert: fs.readFileSync('./certificate.pem'),
-}, app).listen(PORT, HOST, () => {
-  console.log(`Design Bright site running on ${HOST}:${PORT}.`);
+}, app).listen(HTTPS_PORT, HOST, () => {
+  console.log(`Design Bright site running on ${HOST}:${HTTPS_PORT}.`);
 },
 );
--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,8 +1,29 @@
-import axios from 'axios';
 import C from '../constants';
+import auth0 from 'auth0-js';
 
-export const login = loginInfo => (dispatch, getState) => {
-  axios.post('https://192.168.86.200:3001/api/users/login', loginInfo);
+const login = loginInfo => (dispatch, getState) => {
+  const webAuth = new auth0.WebAuth({
+    domain: 'designbright.auth0.com',
+    clientID: 'bBvDRGSmgiYZk2GRZ3Va5hGeuNKwQ3Rh',
+  });
+
+  webAuth.client.login({
+    realm: 'Username-Password-Authentication',
+    username: loginInfo.email,
+    password: loginInfo.password,
+    scope: 'user_metadata',
+  }, (err, authResults) => {
+    if (err) {
+      throw new Error(err);
+    }
+
+    webAuth.client.userInfo(authResults.accessToken, (userErr, user) => {
+      if (userErr) {
+        throw new Error(userErr);
+      }
+      console.log(user);
+    });
+  });
 
   dispatch({
     type: C.LOGIN,
@@ -14,3 +35,5 @@ export const login = loginInfo => (dispatch, getState) => {
     payload: loginInfo,
   };
 };
+
+export default login;
