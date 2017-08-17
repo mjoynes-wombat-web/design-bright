ddebf18c160d1fcb575c816aa1aec332802877f6
ssmith-wombatweb <ssmith@wombatweb.us>
2017-08-16 17:12:12 -0700

    a

--- react-backend/src/app.js
+++ react-backend/src/app.js
@@ -39,6 +39,7 @@ const corsOptions = {
     if (whitelist.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
+      console.log(origin);
       callback(new Error('Not allowed by CORS'));
     }
   },
