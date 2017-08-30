7b6e41604cd094579ab6a79a463f2c936caaef05
ssmith-wombatweb <ssmith@wombatweb.us>
2017-08-29 19:40:19 -0700

    Added Google Analytics in React.

--- client/src/client/index.js
+++ client/src/client/index.js
@@ -3,15 +3,19 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import ReactGA from 'react-ga';
+
 import store from './store';
 import App from './app';
 
 window.React = React;
 window.store = store;
 
+ReactGA.initialize('UA-105647315-1');
+
 render((
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter onUpdate={() => ReactGA.pageview(window.location)}>
       <App />
     </BrowserRouter>
   </Provider>
